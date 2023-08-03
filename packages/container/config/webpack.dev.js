fe36a6775b957536dbd78388a9f644b4d5b7ee32
Rodrigo Oliveira <roliveira.loki@gmail.com>
2023-08-03 16:39:40 -0300

    Initial commint

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { merge } = require('webpack-merge');
+
+const commonConfig = require('./webpack.common');
+const { dependencies } = require('../package.json');
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+      exposes: {
+        './Marketing': './src/bootstrap',
+      },
+      shared: dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
