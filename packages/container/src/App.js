38e7b37168010a749d291066bc315265a539d78c
Rodrigo Oliveira <roliveira.loki@gmail.com>
2023-08-07 12:14:17 -0300

    updated text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Yo Container</h1>
+      <h1>Yo Container!</h1>
       <hr />
       <MarketingApp />;
     </div>
